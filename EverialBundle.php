80be4f4295a8271a13d96f079ceb0ae652fa8be2
Rathgeber Stéphane <stephane.kiora@gmail.com>
2020-03-16 11:28:42 +0100

    load the file service

--- EverialBundle.php
+++ EverialBundle.php
@@ -4,9 +4,13 @@
 namespace Kiora\EverialBundle;
 
 
+use Kiora\EverialBundle\DependencyInjection\EveriralExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EverialBundle extends Bundle
 {
-
+    public function getContainerExtension()
+    {
+        return new EveriralExtension();
+    }
 }
